04442abcade2224432bb8f08d5b1591c53c3c6b0
TekniskSupport <info@teknisksupport.se>
2021-01-16 15:42:44 +0100

    new sensors + does not cash if tomorrow is null
    new sensors + does not cash if tomorrow is null. solves #1

--- get-lowest-n-consecutive-value/get_lowest_value_for_x_consecutive.py
+++ get-lowest-n-consecutive-value/get_lowest_value_for_x_consecutive.py
@@ -1,9 +1,11 @@
 # Configuration
-entity_id = data.get("entity_id")
-target    = data.get("target_entity_id")
-today     = data.get("today")
-tomorrow  = data.get("tomorrow")
-hours     = data.get("hours")
+entity_id       = data.get("entity_id")
+target          = data.get("target_entity_id")
+today_target    = data.get("today_entity_id")
+tomorrow_target = data.get("tomorrow_entity_id")
+today           = data.get("today")
+tomorrow        = data.get("tomorrow")
+hours           = data.get("hours")
 # Price data for today and tomorrow
 prices    = hass.states.get(entity_id)
 today     = prices.as_dict()['attributes'][today]
@@ -33,8 +35,19 @@ def removeTimesPassed(items):
 
 today    = minSum(today,len(today), 'today')
 today    = removeTimesPassed(today)
-tomorrow = minSum(tomorrow,len(tomorrow), 'tomorrow')
-subsets  = today + tomorrow
+if (tomorrow):
+    tomorrow = minSum(tomorrow,len(tomorrow), 'tomorrow')
+    subsets  = today + tomorrow
+
+    state     = sorted(tomorrow, key=lambda k: k.get('sum', 0))
+    state3    = str(tomorrow[0]['hour']) +' '+ tomorrow[0]['day']
+    hass.states.set(tomorrow_target, state3, subsets[0])
+else:
+    subsets  = today
 subsets  = sorted(subsets, key=lambda k: k.get('sum', 0))
 state    = str(subsets[0]['hour']) +' '+ subsets[0]['day']
 hass.states.set(target, state, subsets[0])
+
+subsets  = sorted(today, key=lambda k: k.get('sum', 0))
+state2    = str(subsets[0]['hour']) +' '+ subsets[0]['day']
+hass.states.set(today_target, state2, subsets[0])
